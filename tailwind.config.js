bcc297fb8dd232bac6926605deb1998650388ef1
CosmicSparX <kpranay661@gmail.com>
2024-12-12 13:39:53 +0530

    Inital

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+      content: [
+          "./templates/**/*.html",
+          "./node_modules/flowbite/**/*.js",
+      ],
+      theme: {
+        extend: {
+            colors: {
+                primary: '#FF9B9B',
+                secondary: '#4B5563',
+            },
+        },
+      },
+      plugins: [
+        require('flowbite/plugin'),
+      ]
+}
