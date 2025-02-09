c709c72c4f40fd5590fb9610ba969700fdd86689
CosmicSparX <kpranay661@gmail.com>
2025-02-09 21:28:10 +0530

    hoja madarchod

--- FirstReference/urls.py
+++ FirstReference/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
+from django.urls import path, include
 from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', index, name="index"),
+    path('ads/', include('ad_agency.urls')),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
