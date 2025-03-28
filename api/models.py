a6cf068a445b3b61a1a2b67a04108b3c400bc599
CosmicSparX <kpranay661@gmail.com>
2025-03-29 01:44:20 +0530

    Fixed

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, User
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 
@@ -154,7 +155,7 @@ class OrganizationUser(AbstractBaseUser, PermissionsMixin):
 
 class Applicant(models.Model):
     """Main applicant model containing basic information"""
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone_regex = RegexValidator(
@@ -310,7 +311,7 @@ class VerificationStatus(models.Model):
 
     applicant = models.OneToOneField(Applicant, on_delete=models.CASCADE, related_name='status')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True,
                                     related_name='assigned_verifications')
     notes = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
